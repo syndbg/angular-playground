6792204a36d10950be6c01e47f2314939aaf775b
Anton Antonov <anton.synd.antonov@gmail.com>
2015-05-03 02:32:54 +0300

    Added chapter-11

--- up-and-running/chapter-11/directive-with-scope/app.js
+++ up-and-running/chapter-11/directive-with-scope/app.js
@@ -3,15 +3,8 @@ angular.module('stockMarketApp', [])
     var self = this;
     self.stocks = [
       {name: 'First Stock', price: 100, previous: 220},
-      {name: 'Fourth Stock', price: 400, previous: 420},
       {name: 'Second Stock', price: 140, previous: 120},
       {name: 'Third Stock', price: 110, previous: 110},
+      {name: 'Fourth Stock', price: 400, previous: 420}
     ];
-
-    self.stockTemplate = 'stock.html';
-
-    self.getChange = function(stock) {
-      return Math.ceil(((stock.price - stock.previous) /tha
-          stock.previous) * 100);
-    };
   }]);
